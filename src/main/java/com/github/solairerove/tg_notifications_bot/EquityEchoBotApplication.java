5ead1009f8b7259c719c2849c1ebbc1c1678c8b1
mkryvitskis <mikita.kryvitski@sporty.com>
2024-04-19 23:15:38 +0800

    just initiated project on spring boot, because i don't any other framework to boost up development using java

---
+++ src/main/java/com/github/solairerove/tg_notifications_bot/EquityEchoBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.solairerove.tg_notifications_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquityEchoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquityEchoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/solairerove/tg_notifications_bot/EquityEchoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.solairerove.tg_notifications_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquityEchoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
